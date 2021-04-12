40503459dfec7f7b9d4779312f7c01b4bdfd7242
HeloisaSant <81187975+HeloisaSant@users.noreply.github.com>
2021-04-12 18:20:00 -0300

    Create atividade10.py

---
+++ atividade10.py
@@ -0,0 +1,58 @@
+#1
+def numeros(N)
+  i = 0
+  while (i < N)
+    if (N % 5 == 0 and N % 3 == 0)
+      i += 1
+      print (N)
+      
+
+#2
+def funcao(N)
+  lista = ["a","b","c"]
+    a=imput("digite uma mensagen")
+    b=imput("digite uma mensagem")
+    c=imput("digite uma mensagem")
+
+   return(len(lista[N]))
+
+  
+#3  
+def lista()
+  list = lista.split()
+  list = []
+  i = 0
+  while i < len(list):
+    if int(list[i]) > 5:
+      list.append(list[i])
+      i += 1
+      else:
+        i+=1
+
+return(list)
+
+
+#4
+def menu()
+  menu:
+    if ("a")
+      print("PSG")
+      
+    elif("b")
+      print("BAYERN")
+      
+    else ("d")
+       print("fim do programa")
+       
+
+
+
+
+
+
+
+
+
+
+
+  
